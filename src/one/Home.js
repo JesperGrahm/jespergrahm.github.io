86bc90f0e978883267ace6560929c31cdb588fb9
jespergrahm <jesper.grahm@gmail.com>
2022-11-03 22:03:28 +0100

    fortfarande inte bra men lite bättre

--- src/one/Home.js
+++ src/one/Home.js
@@ -1,18 +1,17 @@
+import { Link } from 'react-router-dom' 
 import '../Home.css';
 import Navbar from '../Navbar'
 import { getSubdomain } from '../helpers'
-import herr from '../images/herr'
-import dam from '../images/dam'
+import herr from '../images/herr.jpg'
+import dam from '../images/dam.jpg'
 
 function Home() {
   return (
     <body className='d-flex flex-column align-items-center'>
         <h1 className='display-1 p-4'>Applikation</h1>
         <div className='container d-flex h-75'>
-        <p>PLONGA BONGA</p>
-        <p>BINGEBONGA</p>
-        <Link to='/item'>
-            <img src={herr} class='img-thumbnail w-50'/>
+          <Link to='/item'>
+             <img src={herr} class='img-thumbnail w-50'/>
           </Link>
           <Link to='/item'>
             <img src={dam} class='img-thumbnail w-50'/>
