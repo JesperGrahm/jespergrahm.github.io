3f6df12ea8755202c39788291c4cc2c9894daa1e
jespergrahm <jesper.grahm@gmail.com>
2022-11-03 21:44:36 +0100

    Fixing remote

--- src/one/Home.js
+++ src/one/Home.js
@@ -1,13 +1,20 @@
 import '../Home.css';
 import Navbar from '../Navbar'
 import { getSubdomain } from '../helpers'
+import herr from '../images/herr'
+import dam from '../images/dam'
 
 function Home() {
   return (
     <body className='d-flex flex-column align-items-center'>
         <h1 className='display-1 p-4'>Applikation</h1>
         <div className='container d-flex h-75'>
-          <Navbar />
+        <Link to='/item'>
+            <img src={herr} class='img-thumbnail w-50'/>
+          </Link>
+          <Link to='/item'>
+            <img src={dam} class='img-thumbnail w-50'/>
+          </Link>
         </div>
     </body>
   );
