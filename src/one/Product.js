fa2ab10f2b7e5b4b0b2036129e4babb7dac35070
jespergrahm <jesper.grahm@gmail.com>
2022-11-24 13:20:08 +0100

    Final v2

--- src/one/Product.js
+++ src/one/Product.js
@@ -58,8 +58,8 @@ function Product() {
   return (
     <body className='d-flex flex-column align-items-center'>
         <div class='d-flex'>
-          <Link to='/two' className='display-1 m-4 text-decoration-none text-dark'>Webshop</Link>
-          <Link to='/two/cart' class='d-table m-auto ms-4'>
+          <Link to='/one' className='display-1 m-4 text-decoration-none text-dark'>Webshop</Link>
+          <Link to='/one/cart' class='d-table m-auto ms-4'>
             <img src={images['cart.png']} class='cartIcon p-1' />
           </Link>
         </div>
